f1129ee39db4b0b8e904b2de3b116b92094a78e0
Peter Jung <peter@jung.ninja>
2024-06-21 08:46:32 +0200

    use ierc20 from zepelin

--- interfaces/IERC20.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.24;
-
-interface IERC20 {
-    function balanceOf(address account) external view returns (uint256);
-}
--- test/OmenThumbnailMapping.t.sol
+++ test/OmenThumbnailMapping.t.sol
@@ -2,44 +2,31 @@
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
+import {ERC20Mock} from "../lib/openzeppelin-contracts/contracts/mocks/token/ERC20Mock.sol";
 import {OmenThumbnailMapping, IERC20} from "../src/OmenThumbnailMapping.sol";
-
-/// @dev Mocked product maker that allows us to easily change balances (liquidity provided by the user).
-contract FixedProductMarketMaker is IERC20 {
-    mapping(address => uint256) private balances;
-
-    function setBalance(address account, uint256 balance) public {
-        balances[account] = balance;
-    }
-
-    function balanceOf(address account) external view override returns (uint256) {
-        return balances[account];
-    }
-}
 
 contract OmenThumbnailMappingTest is Test {
     OmenThumbnailMapping public omenThumbnailMapping;
-    FixedProductMarketMaker public productMarketMaker;
+    ERC20Mock public productMarketMaker;
 
     function setUp() public {
         omenThumbnailMapping = new OmenThumbnailMapping();
-        productMarketMaker = new FixedProductMarketMaker();
+        productMarketMaker = new ERC20Mock();
     }
 
     function testCanNotUpdateImageWithoutProvidedLiquidity() public {
-        productMarketMaker.setBalance(address(this), 0);
         vm.expectRevert();
         omenThumbnailMapping.set(address(productMarketMaker), "Qm123");
     }
 
     function testCanUpdateImageWithAnyFundsInMarketIfThisIsFirstUpdater() public {
-        productMarketMaker.setBalance(address(this), 1);
+        productMarketMaker.mint(address(this), 1);
         omenThumbnailMapping.set(address(productMarketMaker), "Qm123");
         assertEq(omenThumbnailMapping.get(address(productMarketMaker)), "Qm123");
     }
 
     function testCanUpdateImageWithAnyFundsInMarketIfThisIsTheSamePersonAsTheOneWhoChangedTheImageLastTime() public {
-        productMarketMaker.setBalance(address(this), 1);
+        productMarketMaker.mint(address(this), 1);
         omenThumbnailMapping.set(address(productMarketMaker), "Qm123");
         assertEq(omenThumbnailMapping.get(address(productMarketMaker)), "Qm123");
         omenThumbnailMapping.set(address(productMarketMaker), "Qm456");
@@ -47,24 +34,24 @@ contract OmenThumbnailMappingTest is Test {
     }
 
     function testAnotherUserCanNotUpdateWithoutDoubleTheFunds() public {
-        productMarketMaker.setBalance(address(this), 1);
+        productMarketMaker.mint(address(this), 1);
         omenThumbnailMapping.set(address(productMarketMaker), "Qm123");
         assertEq(omenThumbnailMapping.get(address(productMarketMaker)), "Qm123");
 
         address anotherUser = address(0x123);
-        productMarketMaker.setBalance(anotherUser, 1);
+        productMarketMaker.mint(anotherUser, 1);
         vm.prank(anotherUser);
         vm.expectRevert();
         omenThumbnailMapping.set(address(productMarketMaker), "Qm456");
     }
 
     function testAnotherUserCaUpdateWithDoubleTheFunds() public {
-        productMarketMaker.setBalance(address(this), 1);
+        productMarketMaker.mint(address(this), 1);
         omenThumbnailMapping.set(address(productMarketMaker), "Qm123");
         assertEq(omenThumbnailMapping.get(address(productMarketMaker)), "Qm123");
 
         address anotherUser = address(0x123);
-        productMarketMaker.setBalance(anotherUser, 2);
+        productMarketMaker.mint(anotherUser, 2);
         vm.prank(anotherUser);
         omenThumbnailMapping.set(address(productMarketMaker), "Qm456");
         assertEq(omenThumbnailMapping.get(address(productMarketMaker)), "Qm456");
