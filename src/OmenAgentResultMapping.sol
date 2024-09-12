4037930de810c496c2db496aba814fba1dd7ad1c
gabrielfior <fior.gbm@gmail.com>
2024-09-12 13:20:54 -0300

    Adapted contract/test to have multiple IPFS hashes per market

--- src/OmenAgentResultMapping.sol
+++ src/OmenAgentResultMapping.sol
@@ -1,22 +1,29 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.24;
 
-import "forge-std/console.sol";
-//import {EnumerableMap} from "../lib/openzeppelin-contracts/contracts/utils/structs/EnumerableMap.sol";
-
 contract OmenAgentResultMapping {
     event PredictionAdded(address indexed marketAddress, bytes32 image_hash, address indexed changer);
 
-    mapping(address => bytes32) private marketAddressToIPFSHashes;
+    mapping(address => bytes32[]) private marketAddressToIPFSHashes;
 
     /// @dev Get IPFS hash of thumbnail for the given market.
-    function get(address marketAddress) public view returns (bytes32) {
+    function get(address marketAddress) public view returns (bytes32[] memory) {
         return marketAddressToIPFSHashes[marketAddress];
     }
 
     function set(address marketAddress, bytes32 image_hash) public {
-        console.log("%s", marketAddress);
-        marketAddressToIPFSHashes[marketAddress] = image_hash;
+        marketAddressToIPFSHashes[marketAddress].push(image_hash);
         emit PredictionAdded(marketAddress, image_hash, msg.sender);
+    }
+
+    /// @dev Get the total number of hashes for a given market.
+    function getHashCount(address marketAddress) public view returns (uint256) {
+        return marketAddressToIPFSHashes[marketAddress].length;
+    }
+
+    /// @dev Get a specific IPFS hash by index for a given market.
+    function getByIndex(address marketAddress, uint256 index) public view returns (bytes32) {
+        require(index < marketAddressToIPFSHashes[marketAddress].length, "Index out of bounds");
+        return marketAddressToIPFSHashes[marketAddress][index];
     }
 }
--- test/OmenAgentResultMapping.t.sol
+++ test/OmenAgentResultMapping.t.sol
@@ -3,22 +3,52 @@ pragma solidity ^0.8.13;
 
 import {Test} from "forge-std/Test.sol";
 import {OmenAgentResultMapping} from "../src/OmenAgentResultMapping.sol";
-import "forge-std/console.sol";
 
 contract OmenAgentResultMappingTest is Test {
     OmenAgentResultMapping public omenAgentResultMapping;
 
     function setUp() public {
-        console.log("%s", "setup");
         omenAgentResultMapping = new OmenAgentResultMapping();
     }
 
-    function testAddPrediction() public {
-        console.log("%s", address(msg.sender));
-        bytes32 ipfsHash = keccak256(abi.encodePacked("test-string"));
+    function addMockPrediction(string memory input) internal returns (bytes32) {
+        bytes32 ipfsHash = keccak256(abi.encodePacked(input));
         omenAgentResultMapping.set(address(msg.sender), ipfsHash);
+        return ipfsHash;
+    }
+
+    function testGetPrediction() public {
+        bytes32 insertedHash = addMockPrediction("test-input");
         // results
-        bytes32 result = omenAgentResultMapping.get(address(msg.sender));
-        assertEq(result, ipfsHash);
+        bytes32[] memory result = omenAgentResultMapping.get(address(msg.sender));
+        bytes32 firstHash = result[0];
+        assertEq(firstHash, insertedHash);
+    }
+
+    function testGetLength() public {
+        bytes32 insertedHash = addMockPrediction("test-string");
+        uint256 result = omenAgentResultMapping.getHashCount(address(msg.sender));
+        assertEq(result, 1);
+    }
+
+    function testGetByIndex() public {
+        // Add multiple predictions
+        bytes32 hash1 = addMockPrediction("test-string-1");
+        bytes32 hash2 = addMockPrediction("test-string-2");
+        bytes32 hash3 = addMockPrediction("test-string-3");
+
+        // Test retrieval by index
+        bytes32 result1 = omenAgentResultMapping.getByIndex(address(msg.sender), 0);
+        bytes32 result2 = omenAgentResultMapping.getByIndex(address(msg.sender), 1);
+        bytes32 result3 = omenAgentResultMapping.getByIndex(address(msg.sender), 2);
+
+        // Check that the correct hashes are returned
+        assertEq(result1, hash1);
+        assertEq(result2, hash2);
+        assertEq(result3, hash3);
+
+        // Test an invalid index (should revert)
+        vm.expectRevert();
+        omenAgentResultMapping.getByIndex(address(msg.sender), 3); // Out of bounds
     }
 }
